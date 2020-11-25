f9b572290cfe68a6ffe7bd447ec08dd5f2ccaead
RADU EUGEN ADUMITROAIEI <radu0210@edu.easj.dk>
2020-11-25 09:25:05 +0100

    customer

---
+++ Pages/Customer/CreateCustomer.cshtml.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CS10_Project_Fisketorvet_V1.Pages.Customer
+{
+    public class CreateCustomerModel : PageModel
+    {
+        [BindProperty]
+        public Models.Customer Customer
+        {
+            get; set;
+        }
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            Models.Customer.Create(Customer);
+            return RedirectToPage("/Index");
+        }
+    }
+}
